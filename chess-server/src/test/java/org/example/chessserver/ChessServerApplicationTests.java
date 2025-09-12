3372356d733085a8b03f7ac1d40c5c6949acec94
nguyen minh anh <nanh2832003@gmail.com>
2025-09-12 22:45:56 +0700

    tao module backend

---
+++ chess-server/src/test/java/org/example/chessserver/ChessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
